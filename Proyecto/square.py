71dd08b805d2c3c3060d7cb696e730a3b861a68a
25ramyy <ramiro.linares@alumnos.frm.utn.edu.ar>
2021-05-30 16:14:06 -0300

    "creacion clase cuadrado"

--- Proyecto/square.py
+++ Proyecto/square.py
@@ -1,1 +1,15 @@
- 
+ def square(x, y, size, color):
+     #dibuja un cuadrado en turtle con los parametros xy donde empieza size lado del cuadrado
+     #color es el color del cuadrado
+    import turtle
+    turtle.up()
+    turtle.goto(x, y)
+    turtle.down()
+    turtle.color(color)
+    turtle.begin_fill()
+
+    for count in range(4):
+        turtle.forward(size)
+        turtle.left(90)
+
+    turtle.end_fill()

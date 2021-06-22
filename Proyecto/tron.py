3367028aa1183909b31ffe2b297eec0bfa2d809a
Fede-Cappello <federico.cappello@alumnos.frm.utn.edu.ar>
2021-06-22 11:18:24 -0300

    lugarObjeto

--- Proyecto/tron.py
+++ Proyecto/tron.py
@@ -49,6 +49,9 @@ def draw():
     if abs(p2head.x)>200 or abs(p2head.y)>200:
         print('Jugador rojo gana')
         return
+        
+ if ((lugarObjetox-5 <= p1head.x <= lugarObjetox+5)and(lugarObjetoy-5 <= p1head.y <= lugarObjetoy+5)):
+        p1aim.rotate(-90)
 
     p1body.add(p1head)
     p2body.add(p2head)
